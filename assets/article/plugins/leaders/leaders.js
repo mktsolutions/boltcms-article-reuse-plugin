7434fbde1427371d7b4b0b3f312dbf68d039c41a
rorodriguez <rorodriguez@luxoft.com>
2022-09-22 15:49:40 -0500

    DGTLDEV-423353 image property updated

--- assets/article/plugins/leaders/leaders.js
+++ assets/article/plugins/leaders/leaders.js
@@ -6,7 +6,6 @@ ArticleEditor.add('plugin', 'leaders', {
     start: async function() {
         const response = await fetch(`${this.opts.leaders.url}/api/contents?page=1&contentType=${this.opts.leaders.contentType}&status=published`)
         const dataJson = await response.json()
-        console.log(dataJson)
         const items = {}
         
         for (const leader in dataJson) {
@@ -15,7 +14,7 @@ ArticleEditor.add('plugin', 'leaders', {
                 id: dataJson[leader].id,
                 title: dataJson[leader].fieldValues.name,
                 description: dataJson[leader].fieldValues.description,
-                image: dataJson[leader].fieldValues.image.url,
+                photo: dataJson[leader].fieldValues.image.url,
                 command: 'leaders.insert'
             }
             items[leader] = item
@@ -41,14 +40,10 @@ ArticleEditor.add('plugin', 'leaders', {
     insert: function(params, button, name) {
         this.app.popup.close()
 
-        var $nodes = this.dom('#entry');
-        var $spans = $nodes.find('#first-leader')
-        console.log($spans)
-
         this.app.editor.insertContent({
             html:  `
             <div class="item">
-                <div class="item__image html-code" style="background-image: url('${button.params.image}')">
+                <div class="item__image html-code" style="background-image: url('${button.params.photo}')">
                     <div class="social"><i class="icomoon-linkedin-logo"></i></div>
                 </div>
                 <div class="item__name html-code">
