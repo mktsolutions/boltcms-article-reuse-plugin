a186d67cda8eb17dc660b7692cb40bedfa58e40d
rorodriguez <rorodriguez@luxoft.com>
2022-09-22 16:55:09 -0500

    DGTLDEV-423353 leaders plugin updated

--- assets/article/plugins/leaders/leaders.js
+++ assets/article/plugins/leaders/leaders.js
@@ -7,6 +7,7 @@ ArticleEditor.add('plugin', 'leaders', {
         const response = await fetch(`${this.opts.leaders.url}/api/contents?page=1&contentType=${this.opts.leaders.contentType}&status=published`)
         const dataJson = await response.json()
         const items = {}
+        const selectOptions = {}
         
         for (const leader in dataJson) {
             console.log(dataJson[leader].fieldValues)
@@ -18,6 +19,7 @@ ArticleEditor.add('plugin', 'leaders', {
                 command: 'leaders.insert'
             }
             items[leader] = item
+            selectOptions[leader] = dataJson[leader].fieldValues.name
         }
 
         this.app.toolbar.add('leaders', {
@@ -27,12 +29,33 @@ ArticleEditor.add('plugin', 'leaders', {
             blocks: {
                 types: ['layer']
             },
-            params: items
+            params: {
+                items: items,
+                selectOptions: selectOptions
+            }
         })
     },
     popup: function(params, button) {
         this.app.popup.create('leaders', {
-            items: params,
+            footer: {
+                save: {
+                    title: 'Insert',
+                    command: 'leaders.insert',
+                    type: 'primary'
+                },
+                cancel: {
+                    title: 'Cancel',
+                    command: 'popup.close'
+                }
+            },
+            form: {
+                'leadersList': {
+                    type: 'select',
+                    label: 'Choose a leader to insert',
+                    options: params.selectOptions
+                }
+            },
+            leaders: params.items,
         })
         
         this.app.popup.open({ button: button })
@@ -40,17 +63,21 @@ ArticleEditor.add('plugin', 'leaders', {
     insert: function(params, button, name) {
         this.app.popup.close()
 
+        // get form data
+        var data = params.getData()
+        var ledaerPosition = data.leadersList
+
         this.app.editor.insertContent({
             html:  `
             <div class="item">
-                <div class="item__image html-code" style="background-image: url('${button.params.photo}')">
+                <div class="item__image html-code" style="background-image: url('${params.params.leaders[ledaerPosition].photo}')">
                     <div class="social"><i class="icomoon-linkedin-logo"></i></div>
                 </div>
                 <div class="item__name html-code">
-                    <h5>${button.params.title}</h5>
-                    <p>${button.params.description}</p>
+                    <h5>${params.params.leaders[ledaerPosition].title}</h5>
+                    <p>${params.params.leaders[ledaerPosition].description}</p>
                 </div>
-                <div class="twig-code">{% setcontent leader = 'person/${button.params.id}' %}</div>
+                <div class="twig-code">{% setcontent leader = 'person/${params.params.leaders[ledaerPosition].id}' %}</div>
                 <a href="{{ leader.contentlink }}" class="item__link twig-code">
                     <div class="item__image" style="background-image: url('{{ leader.image }}')">
                         <div class="social"><i class="icomoon-linkedin-logo"></i></div>
