fc32455aa2343e1f8819c4ae516aabdc7238a9d3
Roman Hanoshenko <roman.coding@gmail.com>
2022-12-05 11:22:15 +0100

    DGTLDEV-423933 partial

--- assets/article/plugins/leaders/leaders.js
+++ assets/article/plugins/leaders/leaders.js
@@ -108,7 +108,7 @@ ArticleEditor.add('plugin', 'leaders', {
                         </div>
                         <div class="twig-code">{% setcontent leader = 'person/${params.params.leaders[ledaerPosition].id}' %}</div>
                         {% if leader is not empty %}
-                            {{ include('partials/_leader_card.twig', leader) }}
+                            {{ include('partials/_leader_card.twig') }}
                         {% endif %}
                     </div>`
         })
