7f0c751d33377563c3e68af6fc070ad53744aace
vladbotar <vlad.botar@flowmatters.com>
2022-12-07 13:51:41 +0200

    DGTLDEV-added press releases and fixed industries display

--- assets/article/plugins/insights/insights.js
+++ assets/article/plugins/insights/insights.js
@@ -3,136 +3,148 @@ ArticleEditor.iconInsights = '<i class="fa fa-user"></i>';
 
 // Plugin
 ArticleEditor.add("plugin", "insights", {
-  defaults: {
-    url: window.location.origin,
-    contentTypes: {
-      videos: "videos",
-      blogs: "blogs",
-    },
-  },
-  translations: {
-    en: {
-      insights: {
-        insights: "Insights",
-        add: "Insert",
-        cancel: "Cancel",
-        label: "Choose the design you want to insert",
-      },
-      blocks: {
-        insights: "Insights",
-      },
-    },
-  },
-  init: function() {
-      this.elements = {}
-  },
-  start: async function () {
-    const response = await fetch(
-      `${this.opts.insights.url}/api/contents?page=1&contentType%5B%5D=${this.opts.insights.contentTypes.videos}&contentType%5B%5D=${this.opts.insights.contentTypes.blogs}&status=published`
-    )
-    const dataJson = await response.json()
-    const items = {}
-    const selectOptions = { none: "-- NONE --" }
+	defaults: {
+		url: window.location.origin,
+		contentTypes: {
+			videos: "videos",
+			blogs: "blogs",
+			caseStudies: "case-studies",
+			pressReleases: "pr",
+		},
+	},
+	translations: {
+		en: {
+			insights: {
+				insights: "Insights",
+				add: "Insert",
+				cancel: "Cancel",
+				label: "Choose the design you want to insert",
+			},
+			blocks: {
+				insights: "Insights",
+			},
+		},
+	},
+	init: function () {
+		this.elements = {};
+	},
+	start: async function () {
+		const response = await fetch(
+			`${this.opts.insights.url}/api/contents?page=1&contentType%5B%5D=${this.opts.insights.contentTypes.videos}&contentType%5B%5D=${this.opts.insights.contentTypes.pressReleases}&contentType%5B%5D=${this.opts.insights.contentTypes.blogs}&contentType%5B%5D=${this.opts.insights.contentTypes.caseStudies}&status=published`
+		);
+		const dataJson = await response.json();
+		console.log(dataJson);
+		const items = {};
+		const selectOptions = { none: "-- NONE --" };
 
-    for (const single in dataJson) {
-      console.log(dataJson[single].contentType)
-      const item = {
-        id: dataJson[single].id,
-        title: dataJson[single].fieldValues.title,
-        description: dataJson[single].fieldValues.preview_description_text,
-        slug: dataJson[single].fieldValues.slug,
-        photo: window.location.hostname === "127.0.0.1"
-            ? "https://www.luxoft.com/upload/medialibrary/563/behavioral_archetypes.png"
-            : dataJson[single].fieldValues.photo.url,
-        contentType: dataJson[single].contentType
-      }
-      items[single] = item
-      var singleName = dataJson[single].contentType.substring(0, dataJson[single].contentType.length - 1);
-      selectOptions[single] = `${dataJson[single].fieldValues.name} - ${singleName}`;
-    }
+		for (const single in dataJson) {
+			console.log(dataJson[single].contentType);
+			const item = {
+				id: dataJson[single].id,
+				title: dataJson[single].fieldValues.title,
+				description: dataJson[single].fieldValues.preview_description_text,
+				slug: dataJson[single].fieldValues.slug,
+				photo:
+					window.location.hostname === "127.0.0.1"
+						? "https://www.luxoft.com/upload/medialibrary/563/behavioral_archetypes.png"
+						: dataJson[single].fieldValues.photo.url,
+				contentType: dataJson[single].contentType,
+				industry: dataJson[single].taxonomyValues.industries
+					? dataJson[single].taxonomyValues.industries[
+							Object.keys(dataJson[single].taxonomyValues.industries)[0]
+					  ]
+					: "",
+			};
+			items[single] = item;
+			var singleName = dataJson[single].contentType.substring(
+				0,
+				dataJson[single].contentType.length - 1
+			);
+			selectOptions[
+				single
+			] = `${dataJson[single].fieldValues.name} - ${singleName}`;
+		}
 
-    this.elements = {
-      items,
-      selectOptions,
-    }
+		this.elements = {
+			items,
+			selectOptions,
+		};
 
-    this.app.addbar.add("insights", {
-      title: "## blocks.insights ##",
-      icon: ArticleEditor.iconInsights,
-      command: "insights.popup",
-    });
-  },
-  popup: function () {
-    var stack = this.app.popup.create("insights", {
-      title: "## insights.insights ##",
-      width: "400px",
-      command: "addbar.popup",
-      form: {
-        version: {
-          label: "## insights.label ##",
-          type: "select",
-          options: {
-            simple: "Simple",
-            fancy: "Fancy",
-            mixed: "Mixed",
-          },
-        },
-        item1: {
-          type: "select",
-          label: "Choose an item to insert",
-          options: this.elements.selectOptions,
-        },
-        item2: {
-          type: "select",
-          label: "Choose an item to insert",
-          options: this.elements.selectOptions,
-        },
-        item3: {
-          type: "select",
-          label: "Choose an item to insert",
-          options: this.elements.selectOptions,
-        }
-      },
-      footer: {
-        insert: {
-          title: "## insights.add ##",
-          command: "insights.insert",
-          type: "primary",
-        },
-        cancel: { title: "## insights.cancel ##", command: "popup.close" },
-      },
-    });
+		this.app.addbar.add("insights", {
+			title: "## blocks.insights ##",
+			icon: ArticleEditor.iconInsights,
+			command: "insights.popup",
+		});
+	},
+	popup: function () {
+		var stack = this.app.popup.create("insights", {
+			title: "## insights.insights ##",
+			width: "400px",
+			command: "addbar.popup",
+			form: {
+				version: {
+					label: "## insights.label ##",
+					type: "select",
+					options: {
+						simple: "Simple",
+						fancy: "Fancy",
+						mixed: "Mixed",
+					},
+				},
+				item1: {
+					type: "select",
+					label: "Choose an item to insert",
+					options: this.elements.selectOptions,
+				},
+				item2: {
+					type: "select",
+					label: "Choose an item to insert",
+					options: this.elements.selectOptions,
+				},
+				item3: {
+					type: "select",
+					label: "Choose an item to insert",
+					options: this.elements.selectOptions,
+				},
+			},
+			footer: {
+				insert: {
+					title: "## insights.add ##",
+					command: "insights.insert",
+					type: "primary",
+				},
+				cancel: { title: "## insights.cancel ##", command: "popup.close" },
+			},
+		});
 
-    stack.open({ focus: "insights" })
-  },
-  insert: function (stack) {
-    var instance = this._buildInstance(stack)
+		stack.open({ focus: "insights" });
+	},
+	insert: function (stack) {
+		var instance = this._buildInstance(stack);
 
-    if (instance) {
-      this.app.block.add({ instance: instance })
-      this.app.source.toggle()
-      this.app.source.toggle()
-    }
-  },
-  _buildInstance: function (stack, instance) {
-    this.app.popup.close()
+		if (instance) {
+			this.app.block.add({ instance: instance });
+			this.app.source.toggle();
+			this.app.source.toggle();
+		}
+	},
+	_buildInstance: function (stack, instance) {
+		this.app.popup.close();
 
-    var data = stack.getData()
-  
-    var insightsVersion = data.version
-    var item1 = data.item1
-    var item2 = data.item2
-    var item3 = data.item3
-    var items = [item1, item2, item3]
-    var htmlStructure = ``
-    var htmlItems = ``
+		var data = stack.getData();
 
-    if (insightsVersion === 'simple') {
-      
-      for (var item of items) {
-        if (item !== 'none') {
+		var insightsVersion = data.version;
+		var item1 = data.item1;
+		var item2 = data.item2;
+		var item3 = data.item3;
+		var items = [item1, item2, item3];
+		var htmlStructure = ``;
+		var htmlItems = ``;
 
-          htmlItems += `<div class="col html-code">
+		if (insightsVersion === "simple") {
+			for (var item of items) {
+				if (item !== "none") {
+					htmlItems += `<div class="col html-code">
                           <img src="${this.elements.items[item].photo}" alt="">
                           <h4>${this.elements.items[item].title.en}</h4>
                           <p class="gray">${this.elements.items[item].description}</p>
@@ -152,7 +164,7 @@ ArticleEditor.add("plugin", "insights", {
                         <div class="twig-code">{% setcontent item = '${this.elements.items[item].contentType}/${this.elements.items[item].id}' %}</div>
                         {% if item is not empty %}
                           <div class="col twig-code">
-                            <img src="{{ item.photo.url }}" alt="">
+                            <img src="{{ item.photo.url }}" alt="" />
                             <h4>{{ item.title }}</h4>
                             <p class="gray">{{ item.preview_description_text }}</p>
                             <div class="btn-container">
@@ -168,24 +180,25 @@ ArticleEditor.add("plugin", "insights", {
                               </a>
                             </div>
                           </div>
-                        {% endif %}`
-        }
-      }
+                        {% endif %}`;
+				}
+			}
 
-      htmlStructure = `<section class="container-featured"><div class="title">
-                            <h2>Featured insights - simple</h2>
-                            <div class="row">
-                            ${htmlItems}
-                            </div>
-                          </div></section>`
-      
-    } else if (insightsVersion === 'fancy') {
-
-      for (var item of items) {
-        if (item !== 'none') {
-          htmlItems += `<div class="col html-code">
+			htmlStructure = `<section class="container-featured">
+                        <div class="title">
+                          <h2>Featured insights - simple</h2>
+                        </div>
+                        <div class="row">
+                          ${htmlItems}
+                        </div>
+                      </section>`;
+		} else if (insightsVersion === "fancy") {
+			for (var item of items) {
+				if (item !== "none") {
+					console.log(this.elements.items[item]);
+					htmlItems += `<div class="col html-code">
                           <img src="${this.elements.items[item].photo}" alt="">
-                          <h4>Banking</h4>
+                          <h4>${this.elements.items[item].industry}</h4>
                           <h2>${this.elements.items[item].title.en}</h2>
                           <p class="gray">${this.elements.items[item].description}</p>
                           <div class="btn-container">
@@ -205,7 +218,13 @@ ArticleEditor.add("plugin", "insights", {
                         {% if item is not empty %}
                           <div class="col js-stack-cards__item twig-code">
                             <img src="{{ item.photo }}" alt="">
-                            <h4>{{ item.title }}</h4>
+                            {% if item|taxonomies['industries'] is defined %}
+                              <h4>
+                              {% for industry in item|taxonomies['industries'] %}
+                                {{ industry.name }}{% if not loop.last %}, {% endif %}
+                              {% endfor %}
+                              </h4>
+                            {% endif %}
                             <h2>{{ item.title }}</h2>
                             <p class="gray">{{ item.preview_description_text }}</p>
                             <div class="btn-container">
@@ -221,25 +240,26 @@ ArticleEditor.add("plugin", "insights", {
                               </a>
                             </div>
                           </div>
-                        {% endif %}`
-        }
-      }
+                        {% endif %}`;
+				}
+			}
 
-      htmlStructure = `<section class="featured container-xxl"><div class="container">
+			htmlStructure = `<section class="featured container-xxl"><div class="container">
                             <div class="row">
                               <h2 class="featured__title">Featured insights - fancy</h2>
                             </div>
                             <div class="container-xxl js-stack-cards">
                               ${htmlItems}
                             </div>
-                          </div></section>`
-
-    } else if (insightsVersion === 'mixed') {
-
-      for (var item of items) {
-        if (item !== 'none') {
-          var singleName = this.elements.items[item].contentType.substring(0, this.elements.items[item].contentType.length - 1);
-          htmlItems += `<div class="col html-code">
+                          </div></section>`;
+		} else if (insightsVersion === "mixed") {
+			for (var item of items) {
+				if (item !== "none") {
+					var singleName = this.elements.items[item].contentType.substring(
+						0,
+						this.elements.items[item].contentType.length - 1
+					);
+					htmlItems += `<div class="col html-code">
                           <img src="${this.elements.items[item].photo}" alt="" />
                           <p class="body-text-bold">${singleName}</p>
                           <h5>${this.elements.items[item].title.en}</h5>
@@ -261,7 +281,7 @@ ArticleEditor.add("plugin", "insights", {
                         <div class="twig-code">{% setcontent item = '${this.elements.items[item].contentType}/${this.elements.items[item].id}' %}</div>
                         {% if item is not empty %}
                           <div class="col twig-code">
-                            <img src="{{ item.title }}" alt="" />
+                            <img src="{{ item.photo }}" alt="" />
                             <p class="body-text-bold">{{ item.title }}</p>
                             <h5>{{ item.title }}</h5>
                             <div class="btn-container">
@@ -279,24 +299,24 @@ ArticleEditor.add("plugin", "insights", {
                               </a>
                             </div>
                           </div>
-                        {% endif %}`
-        }
-      }
+                        {% endif %}`;
+				}
+			}
 
-      htmlStructure = `<section class="container-insights"><div class="title">
+			htmlStructure = `<section class="container-insights">
+                        <div class="title">
                             <h2>
                               Key insights and featured news - mixed
                             </h2>
-                          </div>
+                        </div>
                           <div class="row">
                             ${htmlItems}
-                          </div></section>`
+                          </div>
+                      </section>`;
+		}
 
-    }
-
-    this.app.editor.insertContent({
-        html: htmlStructure
-    })
-
-  },
-})
+		this.app.editor.insertContent({
+			html: htmlStructure,
+		});
+	},
+});
