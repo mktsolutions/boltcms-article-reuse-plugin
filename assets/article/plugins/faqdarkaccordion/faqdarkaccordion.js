32550841464900ceedfcceb78c117ae28911c372
Luis Angel Acatecatl Reyes <acatecatl123@gmail.com>
2023-08-07 16:29:18 -0600

    DGTLDEV-424813 update name of FAQ Accordions

--- assets/article/plugins/faqdarkaccordion/faqdarkaccordion.js
+++ assets/article/plugins/faqdarkaccordion/faqdarkaccordion.js
@@ -1,36 +1,36 @@
 // Icon
-ArticleEditor.iconAccordion = '<i class="fas fa-stream"></i>'
+ArticleEditor.iconFaqDarkAccordion = '<i class="fas fa-info"></i>'
 
 // Plugin
-ArticleEditor.add('plugin', 'accordion', {
+ArticleEditor.add('plugin', 'faqdarkaccordion', {
     translations: {
         en: {
-            "accordion": {
-                "accordion": "Accordion dark",
+            "faqdarkaccordion": {
+                "faqdarkaccordion": "FAQ dark",
                 "add": "Insert",
                 "cancel": "Cancel",
                 "label": "How many items do you want to insert?",
             },
              "blocks": {
-                 "accordion": "Accordion dark"
+                 "faqdarkaccordion": "FAQ dark"
              }
         }
     },
     start: function() {
-        this.app.addbar.add('accordion', {
-            title: '## blocks.accordion ##',
-            icon: ArticleEditor.iconAccordion,
-            command: 'accordion.popup'
+        this.app.addbar.add('faqdarkaccordion', {
+            title: '## blocks.faqdarkaccordion ##',
+            icon: ArticleEditor.iconFaqDarkAccordion,
+            command: 'faqdarkaccordion.popup'
         })
     },
     popup: function() {
-        var stack = this.app.popup.add('accordion', {
-            title: '## accordion.accordion ##',
+        var stack = this.app.popup.add('faqdarkaccordion', {
+            title: '## faqdarkaccordion.faqdarkaccordion ##',
             width: '400px',
             command: 'addbar.popup',
             form: {
-                accordion: { 
-                    label: '## accordion.label ##',
+                faqdarkaccordion: { 
+                    label: '## faqdarkaccordion.label ##',
                     type: 'select',
                     options: {
                         2: '2',
@@ -46,12 +46,12 @@ ArticleEditor.add('plugin', 'accordion', {
                 }
             },
             footer: {
-                insert: { title: '## accordion.add ##', command: 'accordion.insert', type: 'primary' },
-                cancel: { title: '## accordion.cancel ##', command: 'popup.close' }
+                insert: { title: '## faqdarkaccordion.add ##', command: 'faqdarkaccordion.insert', type: 'primary' },
+                cancel: { title: '## faqdarkaccordion.cancel ##', command: 'popup.close' }
             }
         })
 
-        stack.open({ focus: 'accordion' })
+        stack.open({ focus: 'faqdarkaccordion' })
     },
     insert: function(stack) {
         var instance = this._buildInstance(stack)
@@ -60,23 +60,23 @@ ArticleEditor.add('plugin', 'accordion', {
         this.app.popup.close()
 
         var data = stack.getData()
-        var editableType = data.accordion
+        var editableType = data.faqdarkaccordion
 
         var id = Math.floor(Math.random() * 100)
 
         var stringHtml = 
-                `<section class="accordion-section"><div class="accordion" id="accordionExample${id}">`
+                `<section class="accordion-section"><div class="accordion" id="accordionFaqDark${id}">`
         for(var x = 1; x <= editableType; x++) {
             var itemId = Math.floor(Math.random() * 1000)
             stringHtml += 
-                `<div class="accordion-item">
-                    <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${itemId}" aria-controls="collapse${itemId}">
+                `<div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
+                    <h2 itemprop="name" class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-faq-dark${itemId}" aria-controls="collapse-faq-dark${itemId}">
                         Accordion Item #${x}
                     </button>
                     </h2>
-                    <div id="collapse${itemId}" class="accordion-collapse collapse">
-                    <div class="accordion-body">
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" id="collapse-faq-dark${itemId}" class="accordion-collapse collapse">
+                    <div itemprop="text" class="accordion-body">
                         <strong>This is the first item's accordion body.</strong>
                     </div>
                     </div>
--- assets/article/plugins/faqlightaccordion/faqlightaccordion.js
+++ assets/article/plugins/faqlightaccordion/faqlightaccordion.js
@@ -1,36 +1,36 @@
 // Icon
-ArticleEditor.iconFaqAccordion = '<i class="fas fa-info"></i>'
+ArticleEditor.iconFaqLightAccordion = '<i class="fas fa-feather-alt"></i>'
 
 // Plugin
-ArticleEditor.add('plugin', 'faqaccordion', {
+ArticleEditor.add('plugin', 'faqlightaccordion', {
     translations: {
         en: {
-            "faqaccordion": {
-                "faqaccordion": "FAQ Accordion",
+            "faqlightaccordion": {
+                "faqlightaccordion": "FAQ light",
                 "add": "Insert",
                 "cancel": "Cancel",
                 "label": "How many items do you want to insert?",
             },
              "blocks": {
-                 "faqaccordion": "FAQ Accordion"
+                 "faqlightaccordion": "FAQ light"
              }
         }
     },
     start: function() {
-        this.app.addbar.add('faqaccordion', {
-            title: '## blocks.faqaccordion ##',
-            icon: ArticleEditor.iconFaqAccordion,
-            command: 'faqaccordion.popup'
+        this.app.addbar.add('faqlightaccordion', {
+            title: '## blocks.faqlightaccordion ##',
+            icon: ArticleEditor.iconFaqLightAccordion,
+            command: 'faqlightaccordion.popup'
         })
     },
     popup: function() {
-        var stack = this.app.popup.add('faqaccordion', {
-            title: '## faqaccordion.faqaccordion ##',
+        var stack = this.app.popup.add('faqlightaccordion', {
+            title: '## faqlightaccordion.faqlightaccordion ##',
             width: '400px',
             command: 'addbar.popup',
             form: {
-                faqaccordion: { 
-                    label: '## faqaccordion.label ##',
+                faqlightaccordion: { 
+                    label: '## faqlightaccordion.label ##',
                     type: 'select',
                     options: {
                         2: '2',
@@ -46,12 +46,12 @@ ArticleEditor.add('plugin', 'faqaccordion', {
                 }
             },
             footer: {
-                insert: { title: '## faqaccordion.add ##', command: 'faqaccordion.insert', type: 'primary' },
-                cancel: { title: '## faqaccordion.cancel ##', command: 'popup.close' }
+                insert: { title: '## faqlightaccordion.add ##', command: 'faqlightaccordion.insert', type: 'primary' },
+                cancel: { title: '## faqlightaccordion.cancel ##', command: 'popup.close' }
             }
         })
 
-        stack.open({ focus: 'faqaccordion' })
+        stack.open({ focus: 'faqlightaccordion' })
     },
     insert: function(stack) {
         var instance = this._buildInstance(stack)
@@ -60,22 +60,22 @@ ArticleEditor.add('plugin', 'faqaccordion', {
         this.app.popup.close()
 
         var data = stack.getData()
-        var editableType = data.faqaccordion
+        var editableType = data.faqlightaccordion
 
         var id = Math.floor(Math.random() * 100)
 
         var stringHtml = 
-                `<section class="accordion-section"><div class="accordion" id="accordionExample${id}">`
+                `<section class="accordion-section--light"><div class="accordion" id="accordionFaqLight${id}">`
         for(var x = 1; x <= editableType; x++) {
             var itemId = Math.floor(Math.random() * 1000)
             stringHtml += 
                 `<div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
                     <h2 itemprop="name" class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${itemId}" aria-controls="collapse${itemId}">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-faq-light${itemId}" aria-controls="collapse-faq-light${itemId}">
                         Accordion Item #${x}
                     </button>
                     </h2>
-                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" id="collapse${itemId}" class="accordion-collapse collapse">
+                    <div itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer" id="collapse-faq-light${itemId}" class="accordion-collapse collapse">
                     <div itemprop="text" class="accordion-body">
                         <strong>This is the first item's accordion body.</strong>
                     </div>
