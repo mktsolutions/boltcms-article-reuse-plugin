06897b267e73ee64ca1a747ada5746743a8b1e2b
Rosendo <chendo1988@gmail.com>
2024-02-14 12:39:36 -0600

    DGTLDEV-425248 buttons plugins

--- assets/article/plugins/career/globalButtons/buttons.js
+++ assets/article/plugins/career/globalButtons/buttons.js
@@ -28,6 +28,22 @@ ArticleEditor.add('plugin', 'globalButtons', {
                     type: 'checkbox',
                     text: '<img src="/assets/article/plugins/career/globalButtons/version-4.png">'
                 },
+                'style5': {
+                    type: 'checkbox',
+                    text: '<img src="/assets/article/plugins/career/globalButtons/version-5.png">'
+                },
+                'style6': {
+                    type: 'checkbox',
+                    text: '<img src="/assets/article/plugins/career/globalButtons/version-6.png">'
+                },
+                'style7': {
+                    type: 'checkbox',
+                    text: '<img src="/assets/article/plugins/career/globalButtons/version-7.png">'
+                },
+                'style8': {
+                    type: 'checkbox',
+                    text: '<img src="/assets/article/plugins/career/globalButtons/version-8.png">'
+                },
                 'aligned': { 
                     label: 'Select an alignment:',
                     type: 'select',
@@ -52,28 +68,46 @@ ArticleEditor.add('plugin', 'globalButtons', {
         this.app.popup.close()
 
         var data = stack.getData()
-        var aligned = data.aligned
-        var [style1, style2, style3, style4] = [data.style1, data.style2, data.style3, data.style4]
         var alignedClass = data.aligned === 'center' ? 'btn-wrapper btn-wrapper--center' : 'btn-wrapper btn-wrapper--left'
 
         var htmlContent = `<div class="${alignedClass}">`
 
-        if (style1) {
+        if (data.style1) {
             htmlContent += `<a class="btn-normal" href="#">Button text</a>`
         }
 
-        if (style2) {
+        if (data.style2) {
             htmlContent += `<a class="btn-normal outline" href="#">Button text</a>`
         }
 
-        if (style3) {
+        if (data.style3) {
             htmlContent += `<a class="btn-arrow-ghost--purple filled" href="#">Button text
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewbox="0 0 24 25" fill="none"><path d="M16.2581 8.2417L6.96777 17.532" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path><path d="M10 7.5L16.95 7.549L17 14.5" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path></svg>
                             </a>`
         }
 
-        if (style4) {
+        if (data.style4) {
             htmlContent += `<a class="btn-arrow-ghost--purple" href="#">Button text
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewbox="0 0 24 25" fill="none"><path d="M16.2581 8.2417L6.96777 17.532" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path><path d="M10 7.5L16.95 7.549L17 14.5" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path></svg>
+                            </a>`
+        }
+
+        if (data.style5) {
+            htmlContent += `<a class="btn-normal btn-normal--rich-black" href="#">Button text</a>`
+        }
+
+        if (data.style6) {
+            htmlContent += `<a class="btn-normal btn-normal--rich-black outline" href="#">Button text</a>`
+        }
+
+        if (data.style7) {
+            htmlContent += `<a class="btn-arrow-ghost--rich-black filled" href="#">Button text
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewbox="0 0 24 25" fill="none"><path d="M16.2581 8.2417L6.96777 17.532" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path><path d="M10 7.5L16.95 7.549L17 14.5" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path></svg>
+                            </a>`
+        }
+
+        if (data.style8) {
+            htmlContent += `<a class="btn-arrow-ghost--rich-black" href="#">Button text
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewbox="0 0 24 25" fill="none"><path d="M16.2581 8.2417L6.96777 17.532" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path><path d="M10 7.5L16.95 7.549L17 14.5" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="round"></path></svg>
                             </a>`
         }
