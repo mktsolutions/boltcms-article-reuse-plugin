e40ba9dcd85c12039f731db0f004fd1871707617
Rosendo <chendo1988@gmail.com>
2024-02-22 12:08:31 -0600

    DGTLDEV-425185 blog cta snnippets

--- assets/article/plugins/career/bolt-api/code.js
+++ assets/article/plugins/career/bolt-api/code.js
@@ -3,150 +3,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
     const pagesAmount = 10;
     const pageSize = 200;
 
-    // if (
-    //   localStorage.getItem("contentTypePeopleData") === null &&
-    //   localStorage.getItem("alreadyGettingPeople") === null
-    // ) {
-    //   localStorage.setItem("alreadyGettingPeople", true);
-    //   console.log("GettingPeople...............");
-    //   let leaderPosition = 0;
-    //   let peopleItems = {};
-    //   let peopleSelectOptions = {};
-
-    //   for (let i = 1; i <= pagesAmount; i++) {
-    //     let apiResponse = await fetch(
-    //       `${window.location.origin}/api/contents?page=${i}&contentType=people&status=published&pageSize=${pageSize}`
-    //     );
-    //     let json = await apiResponse.json();
-
-    //     if (!json.length) {
-    //       break;
-    //     }
-
-    //     const result = await getLeadersData(json, leaderPosition);
-    //     peopleItems = {
-    //       ...peopleItems,
-    //       ...result[0],
-    //     };
-    //     peopleSelectOptions = {
-    //       ...peopleSelectOptions,
-    //       ...result[1],
-    //     };
-    //     leaderPosition = result[2];
-
-    //     if (json.length < pageSize) {
-    //       // if no data - no need to send more requests
-    //       break;
-    //     }
-    //   }
-
-    //   const contentTypePeopleData = {
-    //     items: peopleItems,
-    //     selectOptions: peopleSelectOptions,
-    //   };
-
-    //   localStorage.setItem(
-    //     "contentTypePeopleData",
-    //     JSON.stringify(contentTypePeopleData)
-    //   );
-    // }
-
-    // if (
-    //   localStorage.getItem("contentTypeCsData") === null &&
-    //   localStorage.getItem("alreadyGettingCs") === null
-    // ) {
-    //   console.log("GettingCs...............");
-    //   localStorage.setItem("alreadyGettingCs", true);
-    //   let csItems = {};
-    //   let csSelectOptions = { none: "-- NONE --" };
-    //   let csItemPosition = 0;
-
-    //   for (let i = 1; i <= pagesAmount; i++) {
-    //     let apiResponse = await fetch(
-    //       `${window.location.origin}/api/contents?page=${i}&contentType=case-studies&status=published&pageSize=${pageSize}`
-    //     );
-    //     let json = await apiResponse.json();
-
-    //     if (!json.length) {
-    //       break;
-    //     }
-
-    //     const result = await getCsItemsData(json, csItemPosition);
-
-    //     csItems = {
-    //       ...csItems,
-    //       ...result[0],
-    //     };
-    //     csSelectOptions = {
-    //       ...csSelectOptions,
-    //       ...result[1],
-    //     };
-    //     csItemPosition = result[2];
-
-    //     if (json.length < pageSize) {
-    //       // if no data - no need to send more requests
-    //       break;
-    //     }
-    //   }
-
-    //   const csElements = {
-    //     items: csItems,
-    //     selectOptions: csSelectOptions,
-    //   };
-
-    //   localStorage.setItem("contentTypeCsData", JSON.stringify(csElements));
-    // }
-
-    // if (
-    //   localStorage.getItem("contentTypeInsightsData") === null &&
-    //   localStorage.getItem("alreadyGettingInsights") === null
-    // ) {
-    //   console.log("GettingInsights...............");
-    //   localStorage.setItem("alreadyGettingInsights", true);
-    //   let insightsItems = {};
-    //   let insightsSelectOptions = { none: "-- NONE --" };
-    //   let insightPosition = 0;
-
-    //   for (let i = 1; i <= pagesAmount; i++) {
-    //     let apiResponse = await fetch(
-    //       `${window.location.origin}/api/contents?page=${i}&contentType%5B%5D=videos&contentType%5B%5D=pr&contentType%5B%5D=whitepapers&contentType%5B%5D=blog&contentType%5B%5D=case-studies&status=published&pageSize=${pageSize}`
-    //     );
-    //     let json = await apiResponse.json();
-
-    //     if (!json.length) {
-    //       // if no data - no need to send more requests
-    //       break;
-    //     }
-
-    //     const result = await getInsightsItemsData(json, insightPosition);
-
-    //     insightsItems = {
-    //       ...insightsItems,
-    //       ...result[0],
-    //     };
-    //     insightsSelectOptions = {
-    //       ...insightsSelectOptions,
-    //       ...result[1],
-    //     };
-    //     insightPosition = result[2];
-
-    //     if (json.length < pageSize) {
-    //       // if no data - no need to send more requests
-    //       break;
-    //     }
-    //   }
-
-    //   const insightsElements = {
-    //     items: insightsItems,
-    //     selectOptions: insightsSelectOptions,
-    //   };
-
-    //   localStorage.setItem(
-    //     "contentTypeInsightsData",
-    //     JSON.stringify(insightsElements)
-    //   );
-    // }
-
     if (
       localStorage.getItem("contentTypeLocationsData") === null &&
       localStorage.getItem("alreadyGettingLocations") === null
@@ -248,92 +104,94 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }
 
     if (
+      localStorage.getItem('contentTypeBlogsData') === null &&
+      localStorage.getItem('alreadyGettingBlogs') === null
+    ) {
+      console.log('GettingBlogs...............')
+      localStorage.setItem('alreadyGettingBlogs', true);
+      let insightsItems = {}
+      let insightsSelectOptions = { none: "-- NONE --" }
+      let insightPosition = 0
+
+      for (let i = 1; i <= pagesAmount; i++) {
+        let apiResponse = await fetch(
+          `${window.location.origin}/api/contents?page=${i}&contentType%5B%5D=blog&status=published&pageSize=${pageSize}`
+        );
+        let json = await apiResponse.json();
+
+        if (!json.length) {
+          // if no data - no need to send more requests
+          break;
+        }
+
+        const result = await getBlogItemData(json, insightPosition);
+
+        insightsItems = {
+          ...insightsItems,
+          ...result[0],
+        };
+        insightsSelectOptions = {
+          ...insightsSelectOptions,
+          ...result[1],
+        };
+        insightPosition = result[2];
+
+        if (json.length < pageSize) {
+          // if no data - no need to send more requests
+          break;
+        }
+      }
+
+      const insightsElements = {
+        items: insightsItems,
+        selectOptions: insightsSelectOptions
+      }
+
+      localStorage.setItem('contentTypeBlogsData', JSON.stringify(insightsElements))
+    }
+
+    if (
       localStorage.getItem("contentTypeJobsData") === null &&
       localStorage.getItem("alreadyGettingJobs") === null
     ) {
       console.log("GettingJobs...............");
       localStorage.setItem("alreadyGettingJobs", true);
-      let jobsItems = {};
-      let jobsSelectOptions = { none: "-- NONE --" };
-      let jobPosition = 0;
 
       let apiResponse = await fetch("/bolt/api/all-jobs");
       let jobs = await apiResponse.json();
 
-      // for (let i = 1; i <= pagesAmount; i++) {
-
-      //   if (!json.length) {
-      //     // if no data - no need to send more requests
-      //     break;
-      //   }
-
-      //   const result = await getJobsItemsData(json, jobPosition);
-
-      //   jobsItems = {
-      //     ...jobsItems,
-      //     ...result[0],
-      //   };
-      //   jobsSelectOptions = {
-      //     ...jobsSelectOptions,
-      //     ...result[1],
-      //   };
-      //   jobPosition = result[2];
-
-      //   if (json.length < pageSize) {
-      //     // if no data - no need to send more requests
-      //     break;
-      //   }
-      // }
-
-      // const jobsElements = {
-      //   items: jobsItems,
-      //   selectOptions: jobsSelectOptions,
-      // };
       localStorage.setItem("contentTypeJobsData", JSON.stringify(jobs));
     }
   }
 
-  getContetypesData();
+  getContetypesData()
 
-  // async function getInsightsItemsData(dataJson, itemPosition) {
-  //   const newItems = {};
-  //   const newSelectOptions = {};
-  //   const isLocal = window.location.hostname === "127.0.0.1";
+  async function getBlogItemData(dataJson, itemPosition) {
+    const newItems = {};
+    const newSelectOptions = {};
+    const isLocal = window.location.hostname === "127.0.0.1";
 
-  //   for (const single in dataJson) {
-  //     const item = {
-  //       id: dataJson[single].id,
-  //       title: dataJson[single].fieldValues.title,
-  //       description: dataJson[single].fieldValues.preview_description_text,
-  //       slug: dataJson[single].fieldValues.slug,
-  //       photo: isLocal
-  //         ? "https://www.luxoft.com/upload/medialibrary/563/behavioral_archetypes.png"
-  //         : dataJson[single].fieldValues.photo.url,
-  //       contentType: dataJson[single].contentType,
-  //       industry: dataJson[single].taxonomyValues.industries
-  //         ? dataJson[single].taxonomyValues.industries[
-  //             Object.keys(dataJson[single].taxonomyValues.industries)[0]
-  //           ]
-  //         : "",
-  //     };
-  //     newItems[itemPosition] = item;
-  //     let singleName =
-  //       dataJson[single].contentType === "case-studies"
-  //         ? "case study"
-  //         : dataJson[single].contentType === "blog"
-  //         ? dataJson[single].contentType
-  //         : dataJson[single].contentType.substring(
-  //             0,
-  //             dataJson[single].contentType.length - 1
-  //           );
-  //     newSelectOptions[
-  //       itemPosition
-  //     ] = `${dataJson[single].fieldValues.name} - ${singleName}`;
-  //     itemPosition++;
-  //   }
+    for (const single in dataJson) {
+      const item = {
+        id: dataJson[single].id,
+        title: dataJson[single].fieldValues.title,
+        description: dataJson[single].fieldValues.preview_description_text,
+        slug: dataJson[single].fieldValues.slug,
+        photo: isLocal
+          ? "https://www.luxoft.com/upload/medialibrary/563/behavioral_archetypes.png"
+          : dataJson[single].fieldValues.photo.url,
+        contentType: dataJson[single].contentType
+      };
+      newItems[itemPosition] = item;
+      let singleName = dataJson[single].contentType
+      newSelectOptions[
+        itemPosition
+      ] = `${dataJson[single].fieldValues.name} - ${singleName}`;
+      itemPosition++;
+    }
 
-  //   return [newItems, newSelectOptions, itemPosition];
-  // }
+    return [newItems, newSelectOptions, itemPosition];
+  }
 
   async function getLocationsItemsData(dataJson, itemPosition) {
     const newItems = {};
@@ -416,74 +274,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
     return [newItems, newSelectOptions, itemPosition];
   }
 
-  // async function getLeadersData(dataJson, leaderPosition) {
-  //   const newItems = {};
-  //   const newSelectOptions = {};
-  //   const isLocal = window.location.hostname === "127.0.0.1";
-
-  //   for (const leader in dataJson) {
-  //     const item = {
-  //       id: dataJson[leader].id,
-  //       name: dataJson[leader].fieldValues.name,
-  //       title: dataJson[leader].fieldValues.title.en,
-  //       link: dataJson[leader].fieldValues.linkedin_url,
-  //       photo: isLocal
-  //         ? "https://www.luxoft.com/upload/resize_cache/iblock/303/400_0_1/RinoAriganello.jpg"
-  //         : dataJson[leader].fieldValues.image.url,
-  //       command: "leaders.insert",
-  //     };
-  //     newItems[leaderPosition] = item;
-  //     newSelectOptions[
-  //       leaderPosition
-  //     ] = `${dataJson[leader].fieldValues.name} - ${dataJson[leader].fieldValues.title.en}`;
-  //     leaderPosition++;
-  //   }
-
-  //   return [newItems, newSelectOptions, leaderPosition];
-  // }
-
-  // async function getCsItemsData(dataJson, itemPosition) {
-  //   const newItems = {};
-  //   const newSelectOptions = {};
-  //   const isLocal = window.location.hostname === "127.0.0.1";
-
-  //   for (const single in dataJson) {
-  //     const item = {
-  //       id: dataJson[single].id,
-  //       title: dataJson[single].fieldValues.title,
-  //       description: dataJson[single].fieldValues.preview_description_text,
-  //       slug: dataJson[single].fieldValues.slug,
-  //       photo: isLocal
-  //         ? "https://www.luxoft.com/upload/medialibrary/563/behavioral_archetypes.png"
-  //         : dataJson[single].fieldValues.photo.url,
-  //       contentType: dataJson[single].contentType,
-  //       industry: dataJson[single].taxonomyValues.industries
-  //         ? dataJson[single].taxonomyValues.industries[
-  //             Object.keys(dataJson[single].taxonomyValues.industries)[0]
-  //           ]
-  //         : "",
-  //     };
-
-  //     newItems[itemPosition] = item;
-  //     newSelectOptions[itemPosition] = `${dataJson[single].fieldValues.name}`;
-  //     itemPosition++;
-  //   }
-
-  //   return [newItems, newSelectOptions, itemPosition];
-  // }
 });
 
 window.addEventListener("beforeunload", () => {
-  // localStorage.removeItem("alreadyGettingCs");
-  // localStorage.removeItem("alreadyGettingPeople");
-  // localStorage.removeItem("alreadyGettingInsights");
-  localStorage.removeItem("alreadyGettingLocations");
-  localStorage.removeItem("alreadyGettingEvents");
-  localStorage.removeItem("alreadyGettingJobs");
-  // localStorage.removeItem("contentTypePeopleData");
-  // localStorage.removeItem("contentTypeInsightsData");
-  // localStorage.removeItem("contentTypeCsData");
-  localStorage.removeItem("contentTypeLocationsData");
-  localStorage.removeItem("contentTypeEventsData");
-  localStorage.removeItem("contentTypeJobsData");
+  localStorage.removeItem("alreadyGettingLocations")
+  localStorage.removeItem("alreadyGettingEvents")
+  localStorage.removeItem("alreadyGettingJobs")
+  localStorage.removeItem("contentTypeLocationsData")
+  localStorage.removeItem("contentTypeEventsData")
+  localStorage.removeItem("contentTypeJobsData")
+  localStorage.removeItem("alreadyGettingBlogs")
+  localStorage.removeItem("contentTypeBlogsData")
 });
