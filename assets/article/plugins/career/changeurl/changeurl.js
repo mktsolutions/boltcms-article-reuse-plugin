6f1c01875fd6f8964440f8cc60a681d980734409
Luis Angel Acatecatl Reyes <acatecatl123@gmail.com>
2024-06-17 11:12:58 -0600

    DGTLDEV-425666 change url also in view saved version link

--- assets/article/plugins/career/changeurl/changeurl.js
+++ assets/article/plugins/career/changeurl/changeurl.js
@@ -1,9 +1,17 @@
-const recordActions = document.getElementsByClassName('record-actions')
+const elementsWithSpecificClass = document.querySelectorAll('.record-actions')
+const singleSpecificClassElements = Array.from(elementsWithSpecificClass).filter(element => element.classList.length === 1)
 
-if (recordActions.length > 0) {
-    recordActions.forEach(element => {
-        const href = element.getElementsByTagName('a').item(0)
-        const value = href.getAttribute('href')
-        href.setAttribute('href', 'https://career.luxoft.com' + value) 
+singleSpecificClassElements.forEach(element => {
+    const links = element.querySelectorAll('a:not(.action-remove-collection-item)')
+
+    links.forEach(link => {
+        const href = link.getAttribute('href')
+        if (href) {
+            const newHref = href.startsWith('https://') 
+                ? href.replace('https://career-admin.luxoft.com', 'https://career.luxoft.com') 
+                : 'https://career.luxoft.com' + href
+
+            link.setAttribute('href', newHref)
+        }
     })
-}
+})
